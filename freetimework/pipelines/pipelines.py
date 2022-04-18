b39e04e091e606e9632f414436beccc89cd4469a
tanli <tanli@zingfront.com>
2022-04-18 21:55:08 +0800

    解析修改 入库修改

--- freetimework/pipelines/pipelines.py
+++ freetimework/pipelines/pipelines.py
@@ -11,16 +11,18 @@ class FreeTimeWorkPipeline:
         }
         for k, v in item.items():
             default.update({k: v})
-
-        instance, created = FreeTimeWorkModel.get_or_create(
-            channel_id=item["channel_id"],
-            defaults=default)
-        if not created:
-            for k, v in item.items():
-                if hasattr(instance, k):
-                    setattr(instance, k, v)
-            instance.updated_at = datetime.datetime.now()
-            instance.status = 1  # 重置推送状态 0 推送失败 1未推送 2 推送完成
-            instance.save()
-
+        try:
+            instance, created = FreeTimeWorkModel.get_or_create(
+                article_id=item["article_id"],
+                defaults=default)
+            if not created:
+                for k, v in item.items():
+                    if hasattr(instance, k):
+                        setattr(instance, k, v)
+                instance.updated_at = datetime.datetime.now()
+                instance.status = 1  # 重置推送状态 0 推送失败 1未推送 2 推送完成
+                instance.save()
+            print(f"入库：{item['article_id']}")
+        except BaseException as e:
+            print(e)
         return item
